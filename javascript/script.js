40b7fb70aea029fe769ba5270ff99be8fb696624
Zaheer Khan <zaheerrahman0@gmail.com>
2024-11-10 19:34:50 +0500

    home pages

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,9 @@
 const menu_bar = document.querySelector(".menu_bar");
-const menuIcone = document.querySelector("#menu");
+const menuIcone = document.querySelector("#menuIcone");
+const button = document.querySelector(".button button");
 
 menuIcone.addEventListener('click', ()=> {
     menu_bar.classList.toggle('showMenu');
+    menuIcone.classList.toggle('open');
+    button.classList.toggle('remove');
 })
